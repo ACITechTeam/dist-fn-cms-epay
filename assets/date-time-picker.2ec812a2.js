42b3cd90feb571895f6211503862ed32596291cf
fdamin24 <ferdy.damin@ciputralife.com>
2023-01-30 09:58:01 +0700

    update

---
+++ assets/date-time-picker.2ec812a2.js
@@ -0,0 +1,215 @@
+import{_ as s}from"./AppDateTimePicker.f54908c6.js";import{k as u,o as l,b as n,q as i,D as d,w as m,p as o}from"./index.44c393c2.js";import{_ as A}from"./AppCardCode.326698ec.js";import{a as c,V as b}from"./VRow.bc7f6ecb.js";import"./VField.deed3cad.js";import"./index.b1c9643e.js";import"./VInput.875ce075.js";import"./router.45ff8619.js";import"./VImg.dfeb6219.js";import"./position.a51bc332.js";import"./easing.36b781ab.js";import"./vue.runtime.esm-bundler.a0b28fd1.js";import"./VCard.d6d3143f.js";import"./VAvatar.32fa6763.js";import"./VBtn.c7f96a76.js";import"./VDivider.58cfe376.js";const v={__name:"DemoDateTimePickerInline",setup(p){const e=u("");return(r,a)=>(l(),n(s,{modelValue:i(e),"onUpdate:modelValue":a[0]||(a[0]=t=>d(e)?e.value=t:null),label:"Inline",config:{inline:!0}},null,8,["modelValue"]))}},F={__name:"DemoDateTimePickerDisabledRange",setup(p){const e=new Date,r=e.toLocaleString("default",{month:"2-digit"}),a=e.getFullYear(),t=u("");return(_,D)=>(l(),n(s,{modelValue:i(t),"onUpdate:modelValue":D[0]||(D[0]=f=>d(t)?t.value=f:null),label:"Disabled Range",config:{dateFormat:"Y-m-d",disable:[{from:`${i(a)}-${i(r)}-20`,to:`${i(a)}-${i(r)}-25`}]}},null,8,["modelValue","config"]))}},V={__name:"DemoDateTimePickerHumanFriendly",setup(p){const e=u("");return(r,a)=>(l(),n(s,{modelValue:i(e),"onUpdate:modelValue":a[0]||(a[0]=t=>d(e)?e.value=t:null),label:"Human Friendly",config:{altInput:!0,altFormat:"F j, Y",dateFormat:"Y-m-d"}},null,8,["modelValue"]))}},Y={__name:"DemoDateTimePickerRange",setup(p){const e=u("");return(r,a)=>(l(),n(s,{modelValue:i(e),"onUpdate:modelValue":a[0]||(a[0]=t=>d(e)?e.value=t:null),label:"Range",config:{mode:"range"}},null,8,["modelValue"]))}},$={__name:"DemoDateTimePickerMultipleDates",setup(p){const e=u("");return(r,a)=>(l(),n(s,{modelValue:i(e),"onUpdate:modelValue":a[0]||(a[0]=t=>d(e)?e.value=t:null),label:"Multiple Dates",config:{mode:"multiple",dateFormat:"Y-m-d"}},null,8,["modelValue"]))}},R={__name:"DemoDateTimePickerDateAndTime",setup(p){const e=u("");return(r,a)=>(l(),n(s,{modelValue:i(e),"onUpdate:modelValue":a[0]||(a[0]=t=>d(e)?e.value=t:null),label:"Date & TIme",config:{enableTime:!0,dateFormat:"Y-m-d H:i"}},null,8,["modelValue"]))}},w={__name:"DemoDateTimePickerTimePicker",setup(p){const e=u("");return(r,a)=>(l(),n(s,{modelValue:i(e),"onUpdate:modelValue":a[0]||(a[0]=t=>d(e)?e.value=t:null),label:"Time picker",config:{enableTime:!0,noCalendar:!0,dateFormat:"H:i"}},null,8,["modelValue"]))}},M={__name:"DemoDateTimePickerBasic",setup(p){const e=u("");return(r,a)=>(l(),n(s,{modelValue:i(e),"onUpdate:modelValue":a[0]||(a[0]=t=>d(e)?e.value=t:null),label:"Default"},null,8,["modelValue"]))}},H={ts:`<script setup lang="ts">
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const date = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="date"
+    label="Default"
+  />
+</template>
+
+`,js:`<script setup>
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const date = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="date"
+    label="Default"
+  />
+</template>
+
+`},I={ts:`<script setup lang="ts">
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const date = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="date"
+    label="Date & TIme"
+    :config="{ enableTime: true, dateFormat: 'Y-m-d H:i' }"
+  />
+</template>
+`,js:`<script setup>
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const date = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="date"
+    label="Date & TIme"
+    :config="{ enableTime: true, dateFormat: 'Y-m-d H:i' }"
+  />
+</template>
+`},j={ts:`<script setup lang="ts">
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const now = new Date()
+const currentMonth = now.toLocaleString('default', { month: '2-digit' })
+const currentYear = now.getFullYear()
+const date = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="date"
+    label="Disabled Range"
+    :config="{ dateFormat: 'Y-m-d', disable: [{ from: \`\${currentYear}-\${currentMonth}-20\`, to: \`\${currentYear}-\${currentMonth}-25\` }] }"
+  />
+</template>
+`,js:`<script setup>
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const now = new Date()
+const currentMonth = now.toLocaleString('default', { month: '2-digit' })
+const currentYear = now.getFullYear()
+const date = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="date"
+    label="Disabled Range"
+    :config="{ dateFormat: 'Y-m-d', disable: [{ from: \`\${currentYear}-\${currentMonth}-20\`, to: \`\${currentYear}-\${currentMonth}-25\` }] }"
+  />
+</template>
+`},x={ts:`<script setup lang="ts">
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const date = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="date"
+    label="Human Friendly"
+    :config="{ altInput: true, altFormat: 'F j, Y', dateFormat: 'Y-m-d' }"
+  />
+</template>
+`,js:`<script setup>
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const date = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="date"
+    label="Human Friendly"
+    :config="{ altInput: true, altFormat: 'F j, Y', dateFormat: 'Y-m-d' }"
+  />
+</template>
+`},U={ts:`<script setup lang="ts">
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const date = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="date"
+    label="Inline"
+    :config="{ inline: true }"
+  />
+</template>
+`,js:`<script setup>
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const date = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="date"
+    label="Inline"
+    :config="{ inline: true }"
+  />
+</template>
+`},y={ts:`<script setup lang="ts">
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const multipleDate = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="multipleDate"
+    label="Multiple Dates"
+    :config="{ mode: 'multiple', dateFormat: 'Y-m-d' }"
+  />
+</template>
+`,js:`<script setup>
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const multipleDate = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="multipleDate"
+    label="Multiple Dates"
+    :config="{ mode: 'multiple', dateFormat: 'Y-m-d' }"
+  />
+</template>
+`},C={ts:`<script setup lang="ts">
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const dateRange = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="dateRange"
+    label="Range"
+    :config="{ mode: 'range' }"
+  />
+</template>
+`,js:`<script setup>
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const dateRange = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="dateRange"
+    label="Range"
+    :config="{ mode: 'range' }"
+  />
+</template>
+`},h={ts:`<script setup lang="ts">
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const time = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="time"
+    label="Time picker"
+    :config="{ enableTime: true, noCalendar: true, dateFormat: 'H:i' }"
+  />
+</template>
+`,js:`<script setup>
+import AppDateTimePicker from '@core/components/AppDateTimePicker.vue'
+
+const time = ref('')
+<\/script>
+
+<template>
+  <AppDateTimePicker
+    v-model="time"
+    label="Time picker"
+    :config="{ enableTime: true, noCalendar: true, dateFormat: 'H:i' }"
+  />
+</template>
+`},te={__name:"date-time-picker",setup(p){return(e,r)=>{const a=M,t=A,_=w,D=R,f=$,T=Y,k=V,P=F,g=v;return l(),n(b,null,{default:m(()=>[o(c,{cols:"12",md:"6"},{default:m(()=>[o(t,{title:"Basic",code:H},{default:m(()=>[o(a)]),_:1},8,["code"])]),_:1}),o(c,{cols:"12",md:"6"},{default:m(()=>[o(t,{title:"Time Picker",code:h},{default:m(()=>[o(_)]),_:1},8,["code"])]),_:1}),o(c,{cols:"12",md:"6"},{default:m(()=>[o(t,{title:"Date and Time",code:I},{default:m(()=>[o(D)]),_:1},8,["code"])]),_:1}),o(c,{cols:"12",md:"6"},{default:m(()=>[o(t,{title:"Multiple Dates",code:y},{default:m(()=>[o(f)]),_:1},8,["code"])]),_:1}),o(c,{cols:"12",md:"6"},{default:m(()=>[o(t,{title:"Range",code:C},{default:m(()=>[o(T)]),_:1},8,["code"])]),_:1}),o(c,{cols:"12",md:"6"},{default:m(()=>[o(t,{title:"Human Friendly",code:x},{default:m(()=>[o(k)]),_:1},8,["code"])]),_:1}),o(c,{cols:"12",md:"6"},{default:m(()=>[o(t,{title:"Disabled Range",code:j},{default:m(()=>[o(P)]),_:1},8,["code"])]),_:1}),o(c,{cols:"12",md:"6"},{default:m(()=>[o(t,{title:"Inline",code:U},{default:m(()=>[o(g)]),_:1},8,["code"])]),_:1})]),_:1})}}};export{te as default};
---
+++ assets/scopeId.b58ba583.js
@@ -0,0 +1,1 @@
+import{af as o}from"./index.44c393c2.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
